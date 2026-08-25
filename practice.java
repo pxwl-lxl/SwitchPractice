efdb4620c7cff891e8b04c3e1d30bfa74141b4ac
nyanari <paulangelo.binbinon@gmail.com>
2026-08-25 14:27:49 +0800

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,46 @@
+public class practice {
+	public static void main(String[] args) {
+
+	// MY VARIABLES
+
+	String day = "Monday";
+
+	// MY VARIABLES
+
+	switch(day) {
+		case "Sunday": 
+			System.out.print("It is Sunday!");
+			break;
+
+		case "Monday": 
+			System.out.print("It is Monday!");
+			break;
+
+		case "Tuesday": 
+			System.out.print("It is Tuesday!");
+			break;
+
+		case "Wednesday": 
+			System.out.print("It is Wednesday!");
+			break;
+
+		case "Thursday": 
+			System.out.print("It is Thursday!");
+			break;
+
+		case "Friday": 
+			System.out.print("It is Friday!");
+			break;
+
+		case "Saturday": 
+			System.out.print("It is Saturday!");
+			break;
+
+		default:
+			System.out.print("Invalid day!");	
+		
+		}	
+
+
+	}
+}
